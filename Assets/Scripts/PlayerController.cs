d717d1cd3d25e9ae15817c053bb1c60b0ac66789
Xurya <xur1@bxscience.edu>
2020-07-17 12:26:52 -0400

    #13 Movement Scripts and Scene

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("Dependencies")]
+    public Rigidbody rb;
+
+    [Header("Movement")]
+    public float speed = 5f;
+    private float horizontalInput = 0f;
+    private float verticalInput = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 velocity = new Vector3(horizontalInput * speed, 0, verticalInput * speed);
+
+        rb.velocity = velocity;
+    }
+}
