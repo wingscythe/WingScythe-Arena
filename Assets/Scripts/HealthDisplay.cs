8a5706f399c5ab0836a60bf3cb2f9addc9cd6dbc
Eric Tong <eric.tong105@gmail.com>
2020-07-17 14:45:10 -0400

    #10 #11 Temporary UI
    Probably going to change HP Bar

---
+++ Assets/Scripts/HealthDisplay.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthDisplay : MonoBehaviour
+{
+    private int current_health = 10;
+    public Text health_display;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    } 
+
+    // Update is called once per frame
+    void Update()
+    {
+        health_display.text = "HP: " + current_health.ToString();
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            current_health--;
+        }
+    }
+}
