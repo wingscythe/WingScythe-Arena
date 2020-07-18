54ab00b1551602d6b045f56c38580f20d9d9a8a4
AndyZheng430 <zhenga6@bxscience.edu>
2020-07-18 13:25:51 -0400

    Added character movement script to move with animation

--- Assets/Scripts/AnimationController.cs
+++ Assets/Scripts/AnimationController.cs
@@ -17,21 +17,7 @@ public class AnimationController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        horizontalInput = Input.GetAxis("Horizontal");
-        verticalInput = Input.GetAxis("Vertical");
-        
-        if (Input.GetKeyDown(KeyCode.Q)) {
-            anims.SetBool("Attack1", true);
-        }
-        if (Input.GetKeyDown(KeyCode.E)) {
-            Debug.Log("Attack2");
-            anims.SetBool("Attack2", true);
-        }
-        if (Input.GetKeyDown(KeyCode.R)) {
-            Debug.Log("Attack3");
-            anims.SetBool("Attack3", true);
-        }
-        anims.SetFloat("Horizontal_Float", horizontalInput);
-        anims.SetFloat("Vertical_Float", verticalInput);
+        anims.SetFloat("horizontal_float", Input.GetAxis("Horizontal"));
+        anims.SetFloat("vertical_float", Input.GetAxis("Vertical"));
     }
 }
