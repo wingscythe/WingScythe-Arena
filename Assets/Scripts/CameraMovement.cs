98e39547f0362bd55b4eb496210e22122460e435
AndyZheng430 <zhenga6@bxscience.edu>
2020-07-18 18:20:08 -0400

    added an arena environment

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -24,5 +24,6 @@ public class CameraMovement : MonoBehaviour
 
         transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
         playerBody.Rotate(Vector3.up * mouseX);
+        // transform.position.y = Mathf.Lerp(transform.position.y, playerBody.position.y, Time.deltaTime * 10);
     }
 }
