1b33f2d4f1d0beec0477b94bcb1241495a58a547
Eric Tong <eric.tong105@gmail.com>
2020-07-18 17:51:16 -0400

    #11
    Made Cooldown Timer/Animation

--- Assets/Scripts/GUI Scripts/SkillCommands.cs
+++ Assets/Scripts/GUI Scripts/SkillCommands.cs
@@ -5,7 +5,6 @@ using UnityEngine.UI;
 
 public class SkillCommands : MonoBehaviour
 {
-    
     // Start is called before the first frame update
     void Start()
     {
--- Assets/Scripts/GUI Scripts/SkillObject.cs
+++ Assets/Scripts/GUI Scripts/SkillObject.cs
@@ -8,25 +8,40 @@ public class SkillObject : MonoBehaviour
     public KeyCode inputKey;
     public Image skillImage;
     public bool onCooldown = false;
-    public float cooldownTime;
+    public float cooldownDuration = 1f;
     private float startTime;
-    private float timePassed;
+    public float timePassed = 0f;
 
     // Start is called before the first frame update
     void Start()
     {
         startTime = Time.time;
-        skillImage.fillAmount = 1f;
+        skillImage.fillAmount = cooldownDuration;
     }
 
     // Update is called once per frame
     void Update()
     {
-        timePassed = Time.time - startTime;
-        if (Input.GetKey(inputKey) && !onCooldown) {
+        adjustCooldown();
+    }
+
+    void adjustCooldown() 
+    {
+        if (Input.GetKey(inputKey) && !onCooldown)
+        {
+            skillImage.fillAmount = 0f;
             onCooldown = true;
-        } else if (onCooldown) {
-            
+            timePassed = 0f;
+        }
+        if (onCooldown)
+        {
+            timePassed += Time.deltaTime;
+            skillImage.fillAmount = (float)timePassed / cooldownDuration;
+            if (timePassed >= cooldownDuration)
+            {
+                onCooldown = false;
+                timePassed = 0f;
+            }
         }
     }
 }
