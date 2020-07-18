9ae40127a68f6a79eca2e195e0f1401f7af70fb4
AndyZheng430 <zhenga6@bxscience.edu>
2020-07-17 22:55:26 -0400

    Movement Scene: Character animation movement and attacks added
    The character in the movement scene is now able to move around with wsad and attack in 3 different ways using Q, E, R.

---
+++ Assets/Scripts/AnimationController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationController : MonoBehaviour
+{
+    float horizontalInput;
+    float verticalInput;
+
+    public Animator anims;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+        
+        if (Input.GetKeyDown(KeyCode.Q)) {
+            anims.SetBool("Attack1", true);
+        }
+        if (Input.GetKeyDown(KeyCode.E)) {
+            Debug.Log("Attack2");
+            anims.SetBool("Attack2", true);
+        }
+        if (Input.GetKeyDown(KeyCode.R)) {
+            Debug.Log("Attack3");
+            anims.SetBool("Attack3", true);
+        }
+        anims.SetFloat("Horizontal_Float", horizontalInput);
+        anims.SetFloat("Vertical_Float", verticalInput);
+    }
+}
